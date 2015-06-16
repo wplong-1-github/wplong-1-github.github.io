1dde45439c6dffa220ffebfab86ca6787bc6311c
Peilong Wang <wplong.acc.1.ount@gmail.com>
2015-06-16 16:42:09 -0500

    Chapter 2_6 7 8

---
+++ exs_2_6.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  int month = 9, day = 7;
+  std::cout << month << day << std::endl;
+  
+  month = 09, day = 07;
+  std::cout << month << day << std::endl;
+
+}
+  
---
+++ exs_2_7.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "who goes with F\145rgus?\012" << std::endl;
+  std::cout << 3.14e1L << 1024u << 3.14L << std::endl;
+}
